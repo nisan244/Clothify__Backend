315516944d36520de2a9da757baca5151ed2a6a5
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-01-05 19:00:41 +0600

    done

--- Clothify/settings.py
+++ Clothify/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://clothify-backend-bmry.onrender.com', "http://localhost:3000",]
+CSRF_TRUSTED_ORIGINS = ['https://clothify-backend-bmry.onrender.com', 'https://*.127.0.0.1',]
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
